258ad5b61f5de8deee49b7bb05dd725184998957
Monika Mak <monika1986mak@gmail.com>
2024-09-27 12:23:03 +0000

    tets true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cfz6*q9@)$djithwjsk)1)ztnz5ce#!k-0pqh7(4d06pft(ijn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-monikamak-djangoci-ikypwk52fih.ws.codeinstitute-ide.net','.herokuapp.com']
 
